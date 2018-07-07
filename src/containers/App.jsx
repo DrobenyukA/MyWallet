3af33718025d18324d443b1b8d1530eeff6d9a38
DrobenyukA <drobenyuk.a@gmail.com>
2018-07-07 09:58:53 +0300

    made basic auth

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import firebase from 'firebase/app';
+import {auth} from 'firebase';
 
+import appConfig from '../config';
 import logo from '../assets/logo.svg';
 
+// 1. INITIALIZE APP
+firebase.initializeApp(appConfig);
+
+// INSTALL language behavior
+auth().languageCode = 'ua';
+
+// To apply the default browser preference instead of explicitly setting it.
+// firebase.auth().useDeviceLanguage();
+ 
+const provider = new firebase.auth.GoogleAuthProvider();
+
+// Additional adjustments https://firebase.google.com/docs/reference/js/firebase.auth.GoogleAuthProvider#setCustomParameters
+provider.setCustomParameters({
+    'login_hint': 'some-address@example.com'
+});
+
 class App extends Component {
+
+    loginHandler = () => {
+        auth().signInWithPopup(provider).then(function(result) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            console.log('RESPONSE: ', result);
+          }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+            console.log('ERROR: ', error);
+          });
+    }
+
     render() {
         return (
             <div className="App">
@@ -14,6 +54,9 @@ class App extends Component {
                 <p className="App-intro">
                     To get started, edit <code> src / App.js </code> and save to reload. 
                 </p>
+                <div className="text-center">
+                    <button onClick={this.loginHandler}>Login with Google</button>
+                </div>
             </div>
         );
     }
